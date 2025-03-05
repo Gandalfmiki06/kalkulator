d0834726b2aaf51b29624bc2729887f42c485060
Gandalfmiki06 <163037224+Gandalfmiki06@users.noreply.github.com>
2025-03-05 10:51:37 +0100

    Add files via upload

---
+++ wyniki.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+
+
+using namespace std;
+int main()
+{
+    double a, b;
+
+   
+    cout << "Suma = " << a + b << endl;
+
+
+    cout << "Roznica = " << a - b << endl;
+    cout << "Iloczyn = " << a * b << endl;
+    cout << "Iloraz = " << a / b << endl;
+}
