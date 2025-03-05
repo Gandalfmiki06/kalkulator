fa306b36795762455604107ac156e02eb8152eb7
Gandalfmiki06 <163037224+Gandalfmiki06@users.noreply.github.com>
2025-03-05 10:48:55 +0100

    Add files via upload

---
+++ kalkulator.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+
+using namespace std;
+int main()
+{
+    double a, b;
+
+    cout << "Podaj pierwsza liczbe: " << endl;
+    cin >> a;
+
+    cout << "Podaj druga liczbe: " << endl;
+    cin >> b;
+
+
+
+
+}
